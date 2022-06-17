7cddc3c29e66e308b876ce43572769cb10d06995
cjp <junpc97@gmail.com>
2022-06-17 18:01:33 +0800

    can read file now

--- main.c
+++ main.c
@@ -25,8 +25,6 @@ void interprete(char *src)
 static void repl()
 {
   char line[1024];
-  struct lexer l;
-  vm_init(&vm);
   for (;;) {
     printf("> ");
     if (!fgets(line, sizeof(line), stdin)) {
@@ -37,8 +35,54 @@ static void repl()
   }
 }
 
+static char *read_file(const char *filename)
+{
+  FILE *file = fopen(filename, "rb");
+  if (file == NULL) {
+    fprintf(stderr, "Could not open file \"%s\".\n", filename);
+    exit(74);
+  }
+
+  fseek(file, 0L, SEEK_END);
+  size_t fsize = ftell(file);
+  rewind(file);
+
+  char *buf = (char *)malloc(fsize + 1);
+  if (buf == NULL) {
+    fprintf(stderr, "Not enough memory to read \"%s\".\n", filename);
+    exit(74);
+  }
+
+  size_t bytes_read = fread(buf, sizeof(char), fsize, file);
+  if (bytes_read != fsize) {
+    fprintf(stderr, "Could not read file \"%s\".\n", filename);
+    exit(74);
+  }
+
+  buf[bytes_read] = '\0';
+
+  fclose(file);
+  return buf;
+}
+
+static void run_file(const char *filename)
+{
+  char *src = read_file(filename);
+  interprete(src);
+}
+
 int main(int argc, char **argv)
 {
-  repl();
+  vm_init(&vm);
+
+  if (argc == 1) {
+    repl();
+  } else if (argc == 2) {
+    run_file(argv[1]);
+  } else {
+    fprintf(stderr, "Usage: clox [path]\n");
+    exit(64);
+  }
+
   return 0;
 }
